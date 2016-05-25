8ce0a47c71fc8137b9d9e75bbfc586ea88455a60
Chris Holt <lefthandedgoat@gmail.com>
2016-05-25 16:08:15 -0500

    add helpers to clean up tests

--- src/AStar/Program.fs
+++ src/AStar/Program.fs
@@ -11,52 +11,10 @@ open printing
 
 context "A* tests"
 
+//this test is the example from the below blog
+//http://www.redblobgames.com/pathfinding/a-star/introduction.html#dijkstra
 "Example test" &&& fun ctx ->
-  let graph =
-    {
-      width = 10
-      height = 10
-      walls = HashSet<Location>()
-      forests = HashSet<Location>()
-      cameFrom = Dictionary<Location, Location>()
-      costSoFar = Dictionary<Location, int>()
-      path = []
-    }
-
-  // Make "diagram 4" from main article
-  [1 .. 3] |> List.iter (fun x ->
-    [7 .. 8] |> List.iter (fun y ->
-      graph.walls.Add({ x = x; y = y }) |> ignore))
-
-  let add location = graph.forests.Add(location) |> ignore
-  add { x = 3; y = 4; }
-  add { x = 3; y = 5; }
-  add { x = 4; y = 1; }
-  add { x = 4; y = 2; }
-  add { x = 4; y = 3; }
-  add { x = 4; y = 4; }
-  add { x = 4; y = 5; }
-  add { x = 4; y = 6; }
-  add { x = 4; y = 7; }
-  add { x = 4; y = 8; }
-  add { x = 5; y = 1; }
-  add { x = 5; y = 2; }
-  add { x = 5; y = 3; }
-  add { x = 5; y = 4; }
-  add { x = 5; y = 5; }
-  add { x = 5; y = 6; }
-  add { x = 5; y = 7; }
-  add { x = 5; y = 8; }
-  add { x = 6; y = 2; }
-  add { x = 6; y = 3; }
-  add { x = 6; y = 4; }
-  add { x = 6; y = 5; }
-  add { x = 6; y = 6; }
-  add { x = 6; y = 7; }
-  add { x = 7; y = 3; }
-  add { x = 7; y = 4; }
-  add { x = 7; y = 5; }
-
+  let graph = helpers.makeMovementCostExmapleGraph ()
   let start = { x = 1; y = 4; }
   let goal =  { x = 8; y = 5; }
 
@@ -65,12 +23,8 @@ context "A* tests"
   drawGrid graph
   ctx.printn ""
 
-  ctx.printn "graph with results"
-  let results = aStar graph start goal manhattanHeuristic
-  drawGrid results
-  ctx.printn ""
-
   ctx.printn "graph with path"
+  let results = aStar graph start goal manhattanHeuristic
   let graph = { graph with path = reconstructPath graph.cameFrom start goal}
   drawGrid graph
 
---
+++ src/AStar/helpers.fs
@@ -0,0 +1,53 @@
+module helpers
+
+open System.Collections.Generic
+open types
+
+//http://www.redblobgames.com/pathfinding/a-star/introduction.html#dijkstra
+let makeMovementCostExmapleGraph () =
+  let graph =
+    {
+      width = 10
+      height = 10
+      walls = HashSet<Location>()
+      forests = HashSet<Location>()
+      cameFrom = Dictionary<Location, Location>()
+      costSoFar = Dictionary<Location, int>()
+      path = []
+    }
+
+  // Make "diagram 4" from main article
+  [1 .. 3] |> List.iter (fun x ->
+    [7 .. 8] |> List.iter (fun y ->
+      graph.walls.Add({ x = x; y = y }) |> ignore))
+
+  let add location = graph.forests.Add(location) |> ignore
+  add { x = 3; y = 4; }
+  add { x = 3; y = 5; }
+  add { x = 4; y = 1; }
+  add { x = 4; y = 2; }
+  add { x = 4; y = 3; }
+  add { x = 4; y = 4; }
+  add { x = 4; y = 5; }
+  add { x = 4; y = 6; }
+  add { x = 4; y = 7; }
+  add { x = 4; y = 8; }
+  add { x = 5; y = 1; }
+  add { x = 5; y = 2; }
+  add { x = 5; y = 3; }
+  add { x = 5; y = 4; }
+  add { x = 5; y = 5; }
+  add { x = 5; y = 6; }
+  add { x = 5; y = 7; }
+  add { x = 5; y = 8; }
+  add { x = 6; y = 2; }
+  add { x = 6; y = 3; }
+  add { x = 6; y = 4; }
+  add { x = 6; y = 5; }
+  add { x = 6; y = 6; }
+  add { x = 6; y = 7; }
+  add { x = 7; y = 3; }
+  add { x = 7; y = 4; }
+  add { x = 7; y = 5; }
+
+  graph
